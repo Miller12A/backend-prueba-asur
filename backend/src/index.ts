4bd46b21920b5efb03ddf95f28f5dcd260149fe9
Miller12A <moramiller12345@gmail.com>
2025-08-07 23:33:01 -0500

    correccion de permisos

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,10 +14,12 @@ const IP = process.env.IP;
 const swaggerSpec = require('./docs/swagger');
 
 app.use(cors({
-  origin: 'http://localhost:4200',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, 
 }));
+
 
 app.use(express.json());
 app.use('/contacts', contactoRoutes);
